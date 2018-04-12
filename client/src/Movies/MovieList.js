4367a8a33225999f0dab86c6b9d81c9fe880235d
Henry K <mykim1891@hotmail.com>
2018-04-12 16:00:03 -0700

    Installed yarn and other dependencies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import MovieCard from './MovieCard';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -11,9 +11,22 @@ export default class MovieList extends Component {
   }
 
   componentDidMount() {
-    // fill me in with an HTTP Request to `localhost:5000/api/movies`
+    // fill me in with an HTTP Request to `localhost:3333/api/movies`
     this.setState({ movies: [] });
+    this.getMovies();
   }
+
+  // draw out GET function as getMovies() funciton
+  getMovies = () => {
+    axios
+      .get(`http://localhost:3333/api/movies`)
+      .then(response => {
+        this.setState({ movies: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
